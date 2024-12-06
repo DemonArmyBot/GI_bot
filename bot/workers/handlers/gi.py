b83dfb6c301c01c600ff2227b6092aaaf17b5cf8
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2024-12-06 22:12:20 +0000

    pyLint: auto-fixes

--- bot/workers/handlers/gi.py
+++ bot/workers/handlers/gi.py
@@ -250,7 +250,10 @@ def fetch_weapon_detail(weapon: dict, weapon_stats: dict) -> tuple:
         r3 = weapon["r3"]["values"] if weapon.get("r3") else []
         r4 = weapon["r4"]["values"] if weapon.get("r4") else []
         r5 = weapon["r5"]["values"] if weapon.get("r5") else []
-        key = [f"{a}/{b}/{c}/{d}/{e}".split("/None", maxsplit=1)[0] for a, b, c, d, e in itertools.zip_longest(r1, r2, r3, r4, r5)]
+        key = [
+            f"{a}/{b}/{c}/{d}/{e}".split("/None", maxsplit=1)[0]
+            for a, b, c, d, e in itertools.zip_longest(r1, r2, r3, r4, r5)
+        ]
         effects = effects.format(*key)
     img_suf = weapon["images"]["filename_gacha"]
     img = add_background(img_suf, rarity, name)

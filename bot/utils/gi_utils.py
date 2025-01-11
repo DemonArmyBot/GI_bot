eb9be1ba5c9ede7a532c72103bfd9d0cb57366fd
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-11 18:48:55 +0100

    fix formating issues

--- bot/utils/gi_utils.py
+++ bot/utils/gi_utils.py
@@ -249,7 +249,7 @@ async def fetch_weapon_detail(weapon: dict, weapon_stats: dict) -> tuple:
         r4 = weapon["r4"]["values"] if weapon.get("r4") else []
         r5 = weapon["r5"]["values"] if weapon.get("r5") else []
         key = [
-            f'*{f"{a}/{b}/{c}/{d}/{e}".split("/None", maxsplit=1)[0]}*'
+            f'**{f"{a}/{b}/{c}/{d}/{e}".split("/None", maxsplit=1)[0]}**'
             for a, b, c, d, e in itertools.zip_longest(r1, r2, r3, r4, r5)
         ]
         effects = effects.format(*key)
@@ -263,19 +263,17 @@ async def fetch_weapon_detail(weapon: dict, weapon_stats: dict) -> tuple:
             max_main_stat = round(max_main_stat)
         else:
             max_main_stat = f"{round(max_main_stat * 100)}%"
-    caption = f"*{name}*\n"
+    caption = f"**{name}**\n"
     caption += f"{'⭐' * rarity}\n\n"
-    caption += f"*Rarity:* *{'★' * rarity}*\n"
-    caption += f"*Type:* *{typ}*\n"
-    caption += f"*Base ATK:* *{base_atk}* ➜ *{max_base_atk}* _(Lvl {max_level})_\n"
+    caption += f"**Rarity:** `{'★' * rarity}`\n"
+    caption += f"**Type:** `{typ}`\n"
+    caption += f"**Base ATK:** `{base_atk}` ➜ `{max_base_atk}` __(Lvl {max_level})__\n"
     if main_stat:
-        caption += (
-            f"*{main_stat}:* *{base_stat}* ➜ *{max_main_stat}* _(Lvl {max_level})_\n"
-        )
-    caption += f"```{(des[:2000] + '…') if len(des) > 2000 else des}```\n\n"
+        caption += f"**{main_stat}:** `{base_stat}` ➜ `{max_main_stat}` __(Lvl {max_level})__\n"
+    caption += f"`{(des[:2000] + '…') if len(des) > 2000 else des}`\n\n"
     if effects:
-        caption += f"*{effect_name}* +\n"
-        caption += f"{effects}"
+        caption += f"**{effect_name}** +\n"
+        caption += f">{effects}"
 
     return img, caption
 

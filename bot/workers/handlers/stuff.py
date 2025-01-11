f22bb52b87d107ed9de0e416b237f53348b0d782
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-01-11 19:29:33 +0000

    pyLint: auto-fixes

--- bot/workers/handlers/stuff.py
+++ bot/workers/handlers/stuff.py
@@ -2,7 +2,12 @@ from datetime import datetime as dt
 
 from pyrogram.filters import regex
 from pyrogram.handlers import CallbackQueryHandler
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    InputMediaVideo,
+)
 
 from bot import bot, pyro_errors
 from bot.utils.bot_utils import get_json
